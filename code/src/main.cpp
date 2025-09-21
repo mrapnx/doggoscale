323e321b438d3423f405863646a806f97c387879
MR <github2@mailmr.de>
2025-09-21 12:24:45 +0200

    Fehlerbehandlung ergänzt

--- code/src/main.cpp
+++ code/src/main.cpp
@@ -1,5 +1,6 @@
 #include <Arduino.h>
-#include "HX711.h"
+#include <map>
+#include <HX711.h>
 #include <TFT_eSPI.h>
 #include <SPI.h>
 
@@ -8,10 +9,14 @@
 #define HX711_SCK D1 // GPIO5
 
 HX711 scale;
+boolean scalePresent = false;
 
 // Button
 #define TARE_BUTTON D0
-unsigned long lastButtonPress = 0;
+#define BUTTON_2    D3   // TODO: Wenn der ESP aufwacht und der Pin angeschlossen ist, klappt es nicht. Nur, wenn man den Pin nach dem Aufwache nanschließt :/
+//#define BUTTON_3    D4
+std::map<uint8_t, unsigned long> lastButtonPress;
+
 const unsigned long debounceTime = 300; // ms
 boolean       buttonState     = false;
 
@@ -22,6 +27,7 @@ float calibration_factor = -21; // -837.8887096774194; // -21;
 TFT_eSPI tft = TFT_eSPI();  // Pins aus User_Setup.h
 
 boolean getTareButtonState();
+boolean getButtonState(uint8_t buttonPin);
 
 boolean getTareButtonState() {
   boolean newState;
@@ -45,20 +51,33 @@ boolean getTareButtonState() {
   }
 }
 
+boolean getButtonState(uint8_t buttonPin) {
+  boolean newState;
+  newState = digitalRead(buttonPin);
+  if (!newState == lastButtonPress[buttonPin]) {
+    lastButtonPress[buttonPin] = newState;
+    if (lastButtonPress[buttonPin] == 1) {
+      Serial.print("Button ");
+      Serial.print(buttonPin);
+      Serial.println(" gedrückt");
+    }
+    return true;
+  } else {
+    return false;
+  }
+}
+
 // -------------------- Setup --------------------
 void setup() {
   Serial.begin(115200);
   delay(500);
+  Serial.println(" ");
+  Serial.println("Aufgewacht");
 
   // Button
   pinMode(TARE_BUTTON, INPUT);
-
-  // HX711 initialisieren
-  scale.begin(HX711_DT, HX711_SCK);
-  scale.set_scale(calibration_factor);
-  scale.tare();
-
-  Serial.println("HX711 bereit, Nullpunkt gesetzt.");
+  pinMode(BUTTON_2, INPUT);
+//  pinMode(BUTTON_3, INPUT);
 
   // Display initialisieren
   tft.init();
@@ -66,35 +85,55 @@ void setup() {
   tft.fillScreen(TFT_BLACK);
   tft.setTextColor(TFT_WHITE, TFT_BLACK);
   tft.setTextSize(2);
+
+  // HX711 initialisieren
   tft.setCursor(0, 0);
-  tft.println("Waage bereit...");
-  delay(1500);
-  tft.fillScreen(TFT_BLACK);
+  tft.println("Warte auf Waage...");
+  Serial.println("Initialisiere Waage");
+  scale.begin(HX711_DT, HX711_SCK);
+
+  while (!scale.is_ready()) {
+    tft.setCursor(0, 0);
+    Serial.println("Warte auf Waage...");
+    delay(100);
+  }
+
+  scale.set_scale(calibration_factor);
+  scale.tare();
+  scalePresent = true;
+  Serial.println("HX711 bereit, Nullpunkt gesetzt.");
+  tft.setCursor(0, 0);
+  tft.println("Waage bereit...       ");
+
 }
 
 // -------------------- Loop --------------------
 void loop() {
-  float gewicht = scale.get_units(5) / 1000; // Mittelwert über 5 Messungen
-  float raw = scale.read_average(5);
-  float value = scale.get_value(5);
+  if (scalePresent) {
+    float gewicht = scale.get_units(5) / 1000; // Mittelwert über 5 Messungen
+    float raw = scale.read_average(5);
+    float value = scale.get_value(5);
 
-  // Serial Ausgabe
-  Serial.printf("Gewicht: %.1f kg\n", gewicht);
-  Serial.printf("Raw: %.2f \n", raw);
-  Serial.printf("Value: %.1f \n", value);
-  Serial.println(" ");
+    // Serial Ausgabe
+    Serial.printf("Gewicht: %.1f kg\n", gewicht);
+    Serial.printf("Raw: %.2f \n", raw);
+    Serial.printf("Value: %.1f \n", value);
+    Serial.println(" ");
 
-  // TFT Ausgabe
-  tft.fillScreen(TFT_BLACK);
-  tft.setCursor(10, 40, 2);
-  tft.setTextColor(TFT_WHITE, TFT_BLACK);
-  tft.println("Gewicht:");
+    // TFT Ausgabe
+    tft.setTextSize(2);
+    tft.fillScreen(TFT_BLACK);
+    tft.setCursor(10, 40, 2);
+    tft.setTextColor(TFT_WHITE, TFT_BLACK);
+    tft.println("Gewicht:");
 
-  tft.setCursor(10, 80, 2);
-  tft.setTextColor(TFT_GREEN, TFT_BLACK);
-  tft.printf("%.2f kg", gewicht);
+    tft.setCursor(10, 80, 2);
+    tft.setTextColor(TFT_GREEN, TFT_BLACK);
+    tft.printf("%.2f kg", gewicht);
 
-  // --- Button check ---
-  getTareButtonState(); 
-
+    // --- Button check ---
+    getTareButtonState(); 
+    getButtonState(BUTTON_2);
+ //   getButtonState(BUTTON_3);
+  }
 }
